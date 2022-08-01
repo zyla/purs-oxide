7cd71ab35b3b12d7555a0da8133be130146235a2
Maciej Bielecki <zyla@prati.pl>
2022-08-01 18:55:35 +0200

    Add failing test for separator after list

--- src/lexer.rs
+++ src/lexer.rs
@@ -999,4 +999,17 @@ mod tests {
         <eof>
         "###);
     }
+
+    #[test]
+    #[ignore = "not implemented yet"]
+    fn test_layout_list() {
+        assert_snapshot!(print_layout(indoc!("
+            x = [ 1, 2 ]
+            y = 1
+        ")), @r###"
+        x = [ 1, 2 ];
+        y = 1
+        <eof>
+        "###);
+    }
 }
