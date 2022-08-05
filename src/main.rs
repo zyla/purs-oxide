dc10e6729715c5bff5acf40b8d5f6212dc7fc2d7
Maciej Bielecki <zyla@prati.pl>
2022-08-05 17:49:08 +0200

    Rename

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io::{stdin, Read};
 fn main() -> std::io::Result<()> {
     let mut input = String::new();
     stdin().lock().read_to_string(&mut input)?;
-    for tok_info in purr::lexer::lex(&input) {
+    for tok_info in purs_oxide::lexer::lex(&input) {
         match tok_info {
             Ok(tok_info) => println!("{:?}", tok_info),
             Err(err) => println!("{:?}", err),
