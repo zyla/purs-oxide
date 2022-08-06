a8a2086d883a01d782f650ef83734cf18dbf686f
Maciej Bielecki <zyla@prati.pl>
2022-08-06 19:29:45 +0200

    Make snapshots smaller

--- src/parser.rs
+++ src/parser.rs
@@ -7,24 +7,19 @@ use lalrpop_util::ParseError;
 
 lalrpop_mod!(pub parser);
 
-pub fn parse_module(
-    input: &str,
-) -> (
-    Vec<ErrorRecovery<usize, Token, &str>>,
-    Result<Module, ParseError<usize, Token, lexer::Error>>,
-) {
+type ParseResult<'a, T> = (
+    Vec<ErrorRecovery<usize, Token, &'a str>>,
+    Result<T, ParseError<usize, Token, lexer::Error>>,
+);
+
+pub fn parse_module(input: &str) -> ParseResult<Module> {
     let mut errors = vec![];
     let lexer = lexer::lex(input);
     let result = parser::ModuleParser::new().parse(&mut errors, lexer);
     (errors, result)
 }
 
-pub fn parse_type(
-    input: &str,
-) -> (
-    Vec<ErrorRecovery<usize, Token, &str>>,
-    Result<Type, ParseError<usize, Token, lexer::Error>>,
-) {
+pub fn parse_type(input: &str) -> ParseResult<Type> {
     let mut errors = vec![];
     let lexer = lexer::lex(input);
     let result = parser::TypeParser::new().parse(&mut errors, lexer);
@@ -33,34 +28,41 @@ pub fn parse_type(
 
 #[cfg(test)]
 mod tests {
-    use super::{parse_module, parse_type};
     use indoc::indoc;
     use insta::{self, assert_debug_snapshot};
+
+    fn expect_success<T>(output: super::ParseResult<T>) -> T {
+        let (errors, result) = output;
+        assert_eq!(errors, &[]);
+        result.unwrap()
+    }
+
+    fn parse_module(input: &str) -> crate::ast::Module {
+        expect_success(super::parse_module(input))
+    }
+    fn parse_type(input: &str) -> crate::ast::Type {
+        expect_success(super::parse_type(input))
+    }
 
     #[test]
     fn test_module_header() {
         assert_debug_snapshot!(parse_module(indoc!("
         module Foo where
         ")), @r###"
-        (
-            [],
-            Ok(
-                Located(
-                    SourceSpan {
-                        start: 0,
-                        end: 16,
-                    },
-                    Commented(
-                        [],
-                        ModuleInner {
-                            name: Symbol(
-                                "Foo",
-                            ),
-                            exports: None,
-                            declarations: [],
-                        },
+        Located(
+            SourceSpan {
+                start: 0,
+                end: 16,
+            },
+            Commented(
+                [],
+                ModuleInner {
+                    name: Symbol(
+                        "Foo",
                     ),
-                ),
+                    exports: None,
+                    declarations: [],
+                },
             ),
         )
         "###);
@@ -72,58 +74,53 @@ mod tests {
         module Foo where
         x = 1
         ")), @r###"
-        (
-            [],
-            Ok(
-                Located(
-                    SourceSpan {
-                        start: 0,
-                        end: 23,
-                    },
-                    Commented(
-                        [],
-                        ModuleInner {
-                            name: Symbol(
-                                "Foo",
-                            ),
-                            exports: None,
-                            declarations: [
-                                Located(
-                                    SourceSpan {
-                                        start: 17,
-                                        end: 22,
-                                    },
-                                    Commented(
-                                        [],
-                                        ValueDeclaration(
-                                            ValueDeclaration {
-                                                ident: Symbol(
-                                                    "x",
-                                                ),
-                                                expr: [
-                                                    GuardedExpr {
-                                                        guards: [],
-                                                        expr: Located(
-                                                            SourceSpan {
-                                                                start: 21,
-                                                                end: 22,
-                                                            },
-                                                            Literal(
-                                                                Integer(
-                                                                    1,
-                                                                ),
-                                                            ),
-                                                        ),
-                                                    },
-                                                ],
-                                            },
-                                        ),
-                                    ),
-                                ),
-                            ],
-                        },
+        Located(
+            SourceSpan {
+                start: 0,
+                end: 23,
+            },
+            Commented(
+                [],
+                ModuleInner {
+                    name: Symbol(
+                        "Foo",
                     ),
-                ),
+                    exports: None,
+                    declarations: [
+                        Located(
+                            SourceSpan {
+                                start: 17,
+                                end: 22,
+                            },
+                            Commented(
+                                [],
+                                ValueDeclaration(
+                                    ValueDeclaration {
+                                        ident: Symbol(
+                                            "x",
+                                        ),
+                                        expr: [
+                                            GuardedExpr {
+                                                guards: [],
+                                                expr: Located(
+                                                    SourceSpan {
+                                                        start: 21,
+                                                        end: 22,
+                                                    },
+                                                    Literal(
+                                                        Integer(
+                                                            1,
+                                                        ),
+                                                    ),
+                                                ),
+                                            },
+                                        ],
+                                    },
+                                ),
+                            ),
+                        ),
+                    ],
+                },
             ),
         )
         "###);
@@ -132,52 +129,37 @@ mod tests {
     #[test]
     fn test_parse_atomic_type() {
         assert_debug_snapshot!(parse_type("var"), @r###"
-        (
-            [],
-            Ok(
-                Located(
-                    SourceSpan {
-                        start: 0,
-                        end: 3,
-                    },
-                    Var(
-                        Symbol(
-                            "var",
-                        ),
-                    ),
+        Located(
+            SourceSpan {
+                start: 0,
+                end: 3,
+            },
+            Var(
+                Symbol(
+                    "var",
                 ),
             ),
         )
         "###);
         assert_debug_snapshot!(parse_type("\"string\""), @r###"
-        (
-            [],
-            Ok(
-                Located(
-                    SourceSpan {
-                        start: 0,
-                        end: 8,
-                    },
-                    TypeLevelString(
-                        "string",
-                    ),
-                ),
+        Located(
+            SourceSpan {
+                start: 0,
+                end: 8,
+            },
+            TypeLevelString(
+                "string",
             ),
         )
         "###);
         assert_debug_snapshot!(parse_type("42"), @r###"
-        (
-            [],
-            Ok(
-                Located(
-                    SourceSpan {
-                        start: 0,
-                        end: 2,
-                    },
-                    TypeLevelInt(
-                        42,
-                    ),
-                ),
+        Located(
+            SourceSpan {
+                start: 0,
+                end: 2,
+            },
+            TypeLevelInt(
+                42,
             ),
         )
         "###);
@@ -186,39 +168,34 @@ mod tests {
     #[test]
     fn test_parse_complex_type() {
         assert_debug_snapshot!(parse_type("Maybe Int"), @r###"
-        (
-            [],
-            Ok(
+        Located(
+            SourceSpan {
+                start: 0,
+                end: 9,
+            },
+            TypeApp(
                 Located(
                     SourceSpan {
                         start: 0,
-                        end: 9,
+                        end: 5,
                     },
-                    TypeApp(
-                        Located(
-                            SourceSpan {
-                                start: 0,
-                                end: 5,
-                            },
-                            TypeConstructor(
-                                QualifiedName(
-                                    Symbol(
-                                        "Maybe",
-                                    ),
-                                ),
+                    TypeConstructor(
+                        QualifiedName(
+                            Symbol(
+                                "Maybe",
                             ),
                         ),
-                        Located(
-                            SourceSpan {
-                                start: 6,
-                                end: 9,
-                            },
-                            TypeConstructor(
-                                QualifiedName(
-                                    Symbol(
-                                        "Int",
-                                    ),
-                                ),
+                    ),
+                ),
+                Located(
+                    SourceSpan {
+                        start: 6,
+                        end: 9,
+                    },
+                    TypeConstructor(
+                        QualifiedName(
+                            Symbol(
+                                "Int",
                             ),
                         ),
                     ),
@@ -227,60 +204,55 @@ mod tests {
         )
         "###);
         assert_debug_snapshot!(parse_type("Either String Int"), @r###"
-        (
-            [],
-            Ok(
+        Located(
+            SourceSpan {
+                start: 0,
+                end: 17,
+            },
+            TypeApp(
                 Located(
                     SourceSpan {
                         start: 0,
-                        end: 17,
+                        end: 13,
                     },
                     TypeApp(
                         Located(
                             SourceSpan {
                                 start: 0,
-                                end: 13,
+                                end: 6,
                             },
-                            TypeApp(
-                                Located(
-                                    SourceSpan {
-                                        start: 0,
-                                        end: 6,
-                                    },
-                                    TypeConstructor(
-                                        QualifiedName(
-                                            Symbol(
-                                                "Either",
-                                            ),
-                                        ),
-                                    ),
-                                ),
-                                Located(
-                                    SourceSpan {
-                                        start: 7,
-                                        end: 13,
-                                    },
-                                    TypeConstructor(
-                                        QualifiedName(
-                                            Symbol(
-                                                "String",
-                                            ),
-                                        ),
+                            TypeConstructor(
+                                QualifiedName(
+                                    Symbol(
+                                        "Either",
                                     ),
                                 ),
                             ),
                         ),
                         Located(
                             SourceSpan {
-                                start: 14,
-                                end: 17,
+                                start: 7,
+                                end: 13,
                             },
                             TypeConstructor(
                                 QualifiedName(
                                     Symbol(
-                                        "Int",
+                                        "String",
                                     ),
                                 ),
+                            ),
+                        ),
+                    ),
+                ),
+                Located(
+                    SourceSpan {
+                        start: 14,
+                        end: 17,
+                    },
+                    TypeConstructor(
+                        QualifiedName(
+                            Symbol(
+                                "Int",
                             ),
                         ),
                     ),
@@ -289,64 +261,59 @@ mod tests {
         )
         "###);
         assert_debug_snapshot!(parse_type("Array (Maybe Int)"), @r###"
-        (
-            [],
-            Ok(
+        Located(
+            SourceSpan {
+                start: 0,
+                end: 17,
+            },
+            TypeApp(
                 Located(
                     SourceSpan {
                         start: 0,
-                        end: 17,
+                        end: 5,
                     },
-                    TypeApp(
-                        Located(
-                            SourceSpan {
-                                start: 0,
-                                end: 5,
-                            },
-                            TypeConstructor(
-                                QualifiedName(
-                                    Symbol(
-                                        "Array",
-                                    ),
-                                ),
+                    TypeConstructor(
+                        QualifiedName(
+                            Symbol(
+                                "Array",
                             ),
                         ),
+                    ),
+                ),
+                Located(
+                    SourceSpan {
+                        start: 6,
+                        end: 17,
+                    },
+                    Parens(
                         Located(
                             SourceSpan {
-                                start: 6,
-                                end: 17,
+                                start: 7,
+                                end: 16,
                             },
-                            Parens(
+                            TypeApp(
                                 Located(
                                     SourceSpan {
                                         start: 7,
-                                        end: 16,
+                                        end: 12,
                                     },
-                                    TypeApp(
-                                        Located(
-                                            SourceSpan {
-                                                start: 7,
-                                                end: 12,
-                                            },
-                                            TypeConstructor(
-                                                QualifiedName(
-                                                    Symbol(
-                                                        "Maybe",
-                                                    ),
-                                                ),
+                                    TypeConstructor(
+                                        QualifiedName(
+                                            Symbol(
+                                                "Maybe",
                                             ),
                                         ),
-                                        Located(
-                                            SourceSpan {
-                                                start: 13,
-                                                end: 16,
-                                            },
-                                            TypeConstructor(
-                                                QualifiedName(
-                                                    Symbol(
-                                                        "Int",
-                                                    ),
-                                                ),
+                                    ),
+                                ),
+                                Located(
+                                    SourceSpan {
+                                        start: 13,
+                                        end: 16,
+                                    },
+                                    TypeConstructor(
+                                        QualifiedName(
+                                            Symbol(
+                                                "Int",
                                             ),
                                         ),
                                     ),
