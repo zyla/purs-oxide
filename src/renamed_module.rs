fc6a8f733fd38d58dccb7ffacb56d7744cafedc3
Jonatan Borkowski <jonatan@thebo.me>
2023-10-13 02:15:42 +0200

    Auxiliary query to extract imported declid

--- src/renamed_module.rs
+++ src/renamed_module.rs
@@ -1,15 +1,21 @@
+use std::collections::HashSet;
+
 use salsa::DebugWithDb;
 
-use crate::{ast::DeclarationRefKind, symbol::Symbol, Db, ModuleId, ParsedModule};
+use crate::{
+    ast::{DeclarationRefKind, ImportDeclarationKind},
+    symbol::Symbol,
+    Db, ModuleId, ParsedModule,
+};
 
-#[derive(PartialEq, Eq, Clone, Debug, DebugWithDb)]
+#[derive(PartialEq, Eq, Clone, Debug, DebugWithDb, Hash)]
 pub struct DeclId {
     pub namespace: Namespace,
     pub module: ModuleId,
     pub name: Symbol,
 }
 
-#[derive(PartialEq, Eq, Clone, Debug, DebugWithDb)]
+#[derive(PartialEq, Eq, Clone, Debug, DebugWithDb, Hash)]
 pub enum Namespace {
     Class,
     Type,
@@ -28,73 +34,82 @@ pub fn renamed_module(db: &dyn Db, module_id: ModuleId) -> () {
     todo!()
 }
 
-struct ExportedDeclExtractor {
+struct ExportedDeclExtractor<'a> {
+    db: &'a dyn Db,
     module_id: ModuleId,
     exported_decls: Vec<DeclId>,
 }
 
-impl ExportedDeclExtractor {
+impl<'a> ExportedDeclExtractor<'a> {
     fn extract(&mut self, module: &ParsedModule) {
-        match &module.ast.exports {
-            Some(decl_ref_kind) => {
-                let mut iter = decl_ref_kind.iter().peekable();
-                while let Some(ref_decl) = iter.peek().copied() {
-                    use DeclarationRefKind::*;
+        if let Some(decl_ref_kind) = &module.ast.exports {
+            let db = self.db;
+            let mut iter = decl_ref_kind.iter().peekable();
+            while let Some(ref_decl) = iter.peek().copied() {
+                use DeclarationRefKind::*;
 
-                    match **ref_decl {
-                        TypeClass { name } => {
-                            self.exported_decls.push(DeclId {
-                                name,
-                                module: self.module_id.clone(),
-                                namespace: Namespace::Class,
-                            });
-                            iter.next();
-                        }
-                        TypeOp { name } => {
-                            self.exported_decls.push(DeclId {
-                                name,
-                                module: self.module_id.clone(),
-                                namespace: Namespace::Type,
-                            });
-                            iter.next();
-                        }
-                        Type { name, .. } => {
-                            self.exported_decls.push(DeclId {
-                                name,
-                                module: self.module_id.clone(),
-                                namespace: Namespace::Type,
-                            });
-                            iter.next();
-                        }
-                        Value { name } => {
-                            self.exported_decls.push(DeclId {
-                                name,
-                                module: self.module_id.clone(),
-                                namespace: Namespace::Value,
-                            });
-                            iter.next();
-                        }
-                        ValueOp { name } => {
-                            self.exported_decls.push(DeclId {
-                                name,
-                                module: self.module_id.clone(),
-                                namespace: Namespace::Value,
-                            });
-                            iter.next();
-                        }
-                        TypeInstanceRef { name, .. } => {
-                            self.exported_decls.push(DeclId {
-                                name,
-                                module: self.module_id.clone(),
-                                namespace: Namespace::Type,
-                            });
-                            iter.next();
-                        }
-                        Module { name } => todo!("re-export"),
+                match **ref_decl {
+                    TypeClass { name } => {
+                        self.exported_decls.push(DeclId {
+                            name,
+                            module: self.module_id,
+                            namespace: Namespace::Class,
+                        });
+                        iter.next();
+                    }
+                    TypeOp { name } => {
+                        self.exported_decls.push(DeclId {
+                            name,
+                            module: self.module_id,
+                            namespace: Namespace::Type,
+                        });
+                        iter.next();
+                    }
+                    Type { name, .. } => {
+                        self.exported_decls.push(DeclId {
+                            name,
+                            module: self.module_id,
+                            namespace: Namespace::Type,
+                        });
+                        iter.next();
+                    }
+                    Value { name } => {
+                        self.exported_decls.push(DeclId {
+                            name,
+                            module: self.module_id,
+                            namespace: Namespace::Value,
+                        });
+                        iter.next();
+                    }
+                    ValueOp { name } => {
+                        self.exported_decls.push(DeclId {
+                            name,
+                            module: self.module_id,
+                            namespace: Namespace::Value,
+                        });
+                        iter.next();
+                    }
+                    TypeInstanceRef { name, .. } => {
+                        self.exported_decls.push(DeclId {
+                            name,
+                            module: self.module_id,
+                            namespace: Namespace::Type,
+                        });
+                        iter.next();
+                    }
+                    Module { name } => {
+                        let module = crate::parsed_module(db, name);
+                        let mut inner = ExportedDeclExtractor {
+                            db,
+                            module_id: name,
+                            exported_decls: Default::default(),
+                        };
+                        inner.extract(&module);
+                        self.exported_decls.append(&mut inner.exported_decls);
+                        iter.next();
                     }
                 }
             }
-            None => todo!(),
         }
     }
 }
@@ -104,6 +119,7 @@ pub fn exported_decls(db: &dyn Db, module_id: ModuleId) -> Vec<DeclId> {
     let module = crate::parsed_module(db, module_id);
 
     let mut extractor = ExportedDeclExtractor {
+        db,
         module_id,
         exported_decls: Default::default(),
     };
@@ -111,4 +127,84 @@ pub fn exported_decls(db: &dyn Db, module_id: ModuleId) -> Vec<DeclId> {
     extractor.extract(&module);
 
     extractor.exported_decls
+}
+
+pub fn imported_decls(db: &dyn Db, module_id: ModuleId) -> Vec<(Option<ModuleId>, DeclId)> {
+    let module = crate::parsed_module(db, module_id);
+
+    let mut imports: Vec<(Option<ModuleId>, DeclId)> = Vec::new();
+
+    let mut iter = module.ast.imports.iter().peekable();
+    while let Some(import) = iter.peek().copied() {
+        use ImportDeclarationKind::*;
+        match &import.kind {
+            Implicit => {
+                crate::renamed_module::exported_decls(db, import.module)
+                    .iter()
+                    .for_each(|i| imports.push((import.alias, i.clone())));
+
+                iter.next();
+            }
+            Explicit(decls) => {
+                decls
+                    .into_iter()
+                    .map(|i| to_decl_id(import.module, i))
+                    .for_each(|i| imports.push((import.alias, i)));
+
+                iter.next();
+            }
+            Hiding(decls) => {
+                let excluded: HashSet<DeclId> = decls
+                    .into_iter()
+                    .map(|i| to_decl_id(import.module, i))
+                    .collect();
+                crate::renamed_module::exported_decls(db, import.module)
+                    .iter()
+                    .filter(|i| !excluded.contains(i))
+                    .for_each(|i| imports.push((import.alias, i.clone())));
+
+                iter.next();
+            }
+        }
+    }
+
+    imports
+}
+
+fn to_decl_id(module_id: ModuleId, kind: &DeclarationRefKind) -> DeclId {
+    use DeclarationRefKind::*;
+
+    match *kind {
+        TypeClass { name } => DeclId {
+            name,
+            module: module_id,
+            namespace: Namespace::Class,
+        },
+        TypeOp { name } => DeclId {
+            name,
+            module: module_id,
+            namespace: Namespace::Type,
+        },
+        Type { name, .. } => DeclId {
+            name,
+            module: module_id,
+            namespace: Namespace::Type,
+        },
+        Value { name } => DeclId {
+            name,
+            module: module_id,
+            namespace: Namespace::Value,
+        },
+        ValueOp { name } => DeclId {
+            name,
+            module: module_id,
+            namespace: Namespace::Value,
+        },
+        TypeInstanceRef { name, .. } => DeclId {
+            name,
+            module: module_id,
+            namespace: Namespace::Type,
+        },
+        Module { name } => todo!("What's here?"),
+    }
 }
