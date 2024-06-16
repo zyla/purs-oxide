192f60d0a7d8a442d8ed6a5627e9eec001dd8739
Jonatan Borkowski <jonatan@thebo.me>
2024-06-16 20:48:54 +0200

    Format diagnostics output (#9)

--- tests/program_tests.rs
+++ tests/program_tests.rs
@@ -21,3 +21,23 @@ fn test_compile_pass(input_filename: &str) -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[test_resources("tests/purs/compile_fail/*.purs")]
+fn test_compile_fail(input_filename: &str) -> anyhow::Result<()> {
+    env_logger::builder().is_test(true).try_init()?;
+
+    let source = std::fs::read_to_string(input_filename)?;
+    let db = &mut purs_oxide::Database::new();
+
+    let module_id = db.add_source_file(input_filename.into(), source)?;
+    let errors: Vec<_> =
+        purs_oxide::typecheck::typecheck_module::accumulated::<Diagnostics>(db, module_id)
+            .iter_mut()
+            .map(|d| d.pp(db))
+            .collect();
+
+    errors.clone().into_iter().for_each(|d| println!("{}", d));
+    assert_ne!(errors, vec![]);
+
+    Ok(())
+}
