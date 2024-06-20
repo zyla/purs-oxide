fcebc6f0b58a1ebb8c0365afae7a876d0672e126
Jonatan Borkowski <jonatan@thebo.me>
2024-06-20 21:53:20 +0200

    Add purescript-prelude as submodule

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,23 @@
+use anyhow::format_err;
+use std::path::PathBuf;
+use std::{fs::File, io::Read};
+
+pub fn load_files(db: &mut crate::Database, files: Vec<PathBuf>) {
+    for filename in files {
+        if let Err(err) = process_file(db, filename.clone()) {
+            println!("FAIL {} error: {}", filename.to_string_lossy(), err);
+        }
+    }
+}
+
+pub fn process_file(db: &mut crate::Database, filename: PathBuf) -> anyhow::Result<()> {
+    let mut contents = String::new();
+    File::open(&filename)?.read_to_string(&mut contents)?;
+    db.add_source_file(filename, contents)
+        .map_err(|_| format_err!("Invalid module name"))?;
+    Ok(())
+}
+
 pub mod tests {
     pub trait DropSalsaId {
         fn drop_salsa_id(&mut self) -> Self;
